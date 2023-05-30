8bc32bde0039a8b804f6eec4f1928da5268d9dbc
NAZIFA TASNEEM <127623130+nazifatasneem2001@users.noreply.github.com>
2023-05-30 17:08:03 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var $a;
+$a=30;
+console.log($a);
+
+var MyName, myName;
+MyName="abcd";
+myName="efgh";
+console.log(MyName);
+console.log(myName);
+var a=20;
+console.log(a);
+var b=20;
+console.log(a);
